d21f32e839c5c83545cefdda0fca64cec404a374
Sersseras <daan.kreutz@web.de>
2024-07-06 12:37:29 +0200

    adad

--- js/layers.js
+++ js/layers.js
@@ -1,87 +1,159 @@
-addLayer("alvi", {
-    symbol: "A",
-    position: 0,
-    startData() {
-        return {
-            unlocked: true,
-            points: new Decimal(0),
-        }
+addLayer("Numbers", {
+  symbol: "N",
+  position: 0,
+  startData() {
+    return {
+      unlocked: true,
+      points: new Decimal(0),
+    };
+  },
+  color: "#00FF00",
+  requires: new Decimal(0),
+  resource: "{}",
+  baseResource: "points",
+  baseAmount() {
+    return player.points;
+  },
+  type: "static",
+  getResetGain() {
+    return 1;
+  },
+  prestigeButtonText() {
+    return "Reset for " + format(getResetGain(this.layer)) + " {}";
+  },
+  row: 0,
+  hotkeys: [
+    {
+      key: "n",
+      description: "n: Reset for {}",
+      onPress() {
+        if (canReset(this.layer)) doReset(this.layer);
+      },
     },
-    color: "#FF0000",
-    requires: new Decimal(10),
-    resource: "alvi points",
-    baseResource: "points",
-    baseAmount() { return player.points },
-    type: "normal",
-    getResetGain() {
-        //multiplying
-        mult = new Decimal(1)
-        if (hasUpgrade(this.layer, 13)) mult = mult.times(upgradeEffect(this.layer, 13))
-
-        return player.points.times(0.1).pow(0.5).times(mult)
+  ],
+  upgrades: {},
+  buyables: {
+    11: {
+      title: "0",
+      cost(x) {
+        return new Decimal(1);
+      },
+      display() {
+        return (
+          "You have " + getBuyableAmount(this.layer, this.id) + " ∅\nCost: {}"
+        );
+      },
+      canAfford() {
+        return player[this.layer].points.gte(this.cost());
+      },
+      buy() {
+        player[this.layer].points = player[this.layer].points.sub(this.cost());
+        setBuyableAmount(
+          this.layer,
+          this.id,
+          getBuyableAmount(this.layer, this.id).add(1)
+        );
+      },
     },
-    prestigeButtonText() { return "Reset for " + format(getResetGain(this.layer)) + " Alvi points\nFormula:(x/10)^(1/2)" },
-    row: 0,
-    hotkeys: [
-        { key: "a", description: "A: Reset for Alvi points", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
-    ],
-    upgrades: {
-        11: {
-            title: "Start",
-            description: "Gain 1 point per second",
-            cost: new Decimal(1),
-            effect() {
-                return 1
-            },
-            effectDisplay() { return "+" + format(upgradeEffect(this.layer, this.id)) },
-        },
-        12: {
-            title: "Start more you Naischer",
-            description: "Multiply point gain based on Alvi points\nFormula: x^(1/2)+1",
-            cost: new Decimal(1),
-            effect() {
-                return player[this.layer].points.pow(0.5).add(1)
-            },
-            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" },
-            unlocked() { return hasUpgrade(this.layer, 11) }
-        },
-        13: {
-            title: "More Alvi is always better",
-            description: "Multiply Alvi point gain based on points\nFormula: ln(x+1)",
-            cost: new Decimal(10),
-            effect() {
-                return player.points.add(1).ln()
-            },
-            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" },
-            unlocked() { return hasUpgrade(this.layer, 12) }
-        },
-        14: {
-            title: "Why, when I start a Modding Tree, it is always you four?",
-            description: "Multiply point gain based on points\nFormula: log(x+1)+1",
-            cost: new Decimal(25),
-            effect() {
-                return player.points.add(1).log(10).add(1)
-            },
-            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" },
-            unlocked() { return hasUpgrade(this.layer, 13) }
-        }
+    12: {
+      title: "1",
+      cost(x) {
+        return new Decimal(1);
+      },
+      display() {
+        return (
+          "You have " +
+          getBuyableAmount(this.layer, this.id) +
+          " {∅}\nCost: 0 and {}"
+        );
+      },
+      canAfford() {
+        return (
+          player[this.layer].points.gte(this.cost()) &&
+          getBuyableAmount(this.layer, 11).gte(this.cost())
+        );
+      },
+      buy() {
+        player[this.layer].points = player[this.layer].points.sub(this.cost());
+        setBuyableAmount(
+          this.layer,
+          11,
+          getBuyableAmount(this.layer, 11).sub(this.cost())
+        );
+        setBuyableAmount(
+          this.layer,
+          this.id,
+          getBuyableAmount(this.layer, this.id).add(1)
+        );
+      },
+      unlocked() {
+        return (
+          getBuyableAmount(this.layer, 11).gte(this.cost()) ||
+          getBuyableAmount(this.layer, this.id).gte(1) ||
+          getBuyableAmount(this.layer, 13).gte(1)
+        );
+      },
     },
-    layerShown() { return true }
-})
+    13: {
+      title: "2",
+      cost(x) {
+        return new Decimal(1);
+      },
+      display() {
+        return (
+          "You have " +
+          getBuyableAmount(this.layer, this.id) +
+          " {∅, {∅}}\nCost: 0, 1 and {}"
+        );
+      },
+      canAfford() {
+        return (
+          player[this.layer].points.gte(this.cost()) &&
+          getBuyableAmount(this.layer, 11).gte(this.cost()) &&
+          getBuyableAmount(this.layer, 12).gte(this.cost())
+        );
+      },
+      buy() {
+        player[this.layer].points = player[this.layer].points.sub(this.cost());
+        setBuyableAmount(
+          this.layer,
+          11,
+          getBuyableAmount(this.layer, 11).sub(this.cost())
+        );
+        setBuyableAmount(
+          this.layer,
+          12,
+          getBuyableAmount(this.layer, 12).sub(this.cost())
+        );
+        setBuyableAmount(
+          this.layer,
+          this.id,
+          getBuyableAmount(this.layer, this.id).add(1)
+        );
+      },
+      unlocked() {
+        return (
+          getBuyableAmount(this.layer, 12).gte(this.cost()) ||
+          getBuyableAmount(this.layer, this.id).gte(1)
+        );
+      },
+    },
+  },
+  layerShown() {
+    return true;
+  },
+});
 
 addLayer("achievements", {
-    symbol: "O",
-    startData() {
-        return {
-            unlocked: true,
-        }
-    },
-    row: "side",
-    tooltip() { return "Achievements" },
-    achievements: {
-        11: {
-            name: "Start",
-            done() { return hasUpgrade('alvi', 11) },
-        }
-    },
-})
+  symbol: "O",
+  startData() {
+    return {
+      unlocked: true,
+    };
+  },
+  row: "side",
+  tooltip() {
+    return "Achievements";
+  },
+  achievements: {},
+});
--- js/mod.js
+++ js/mod.js
@@ -1,88 +1,74 @@
 let modInfo = {
-	name: "The Int Tree",
-	id: "anans",
-	author: "Alvi Deiectiones",
-	pointsName: "points",
-	modFiles: ["layers.js", "tree.js"],
+  name: "Breplicanti",
+  id: "anans",
+  author: "Alvi Deiectiones",
+  pointsName: "Bres",
+  modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
-	discordLink: "",
-	initialStartPoints: new Decimal(10), // Used for hard resets and new players
-	offlineLimit: 0,  // In hours
-}
+  discordName: "",
+  discordLink: "",
+  initialStartPoints: new Decimal(10), // Used for hard resets and new players
+  offlineLimit: 0, // In hours
+};
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
-}
+  num: "0.0",
+  name: "Literally nothing",
+};
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
 		- Added things.<br>
-		- Added stuff.`
+		- Added stuff.`;
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You have reached the end and beaten this game, but for now...`;
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
-var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"];
 
 function getStartPoints() {
-	return new Decimal(modInfo.initialStartPoints)
+  return new Decimal(modInfo.initialStartPoints);
 }
 
 // Determines if it should show points/sec
 function canGenPoints() {
-	return true
+  return true;
 }
 
 // Calculate points/sec!
 function getPointGen() {
-	if (!canGenPoints())
-		return new Decimal(0)
+  if (!canGenPoints()) return new Decimal(0);
 
-	let gain = new Decimal(0)
-	// adding
-	if (hasUpgrade('alvi', 11)) gain = gain.add(1)
+  let gain = new Decimal(0);
 
-	//multiplying
-	if (hasUpgrade('alvi', 12)) gain = gain.times(upgradeEffect('alvi', 12))
-	if (hasUpgrade('alvi', 14)) gain = gain.times(upgradeEffect('alvi', 14))
-
-	return gain
+  return gain;
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
 function addedPlayerData() {
-	return {
-	}
+  return {};
 }
 
 // Display extra things at the top of the page
-var displayThings = [
-]
+var displayThings = [];
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+  return player.points.gte(new Decimal("e280000000"));
 }
-
-
 
 // Less important things beyond this point!
 
 // Style for the background, can be a function
-var backgroundStyle = {
-
-}
+var backgroundStyle = {};
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return (3600) // Default is 1 hour which is just arbitrarily large
+  return 3600; // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
 // you can cap their current resources with this.
-function fixOldSave(oldVersion) {
-}
+function fixOldSave(oldVersion) {}
